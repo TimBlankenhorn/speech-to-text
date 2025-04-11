f46521b01ccd46a6b7272ba784aff140d7c3571b
Tim <Timblankenhorn03@gmail.com>
2025-04-11 13:55:36 +0000

    changed design

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,8 +1,36 @@
 <template>
-  <v-file-upload density="comfortable"></v-file-upload>
-
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" sm="8" md="6">
+        <v-card elevation="2" class="pa-4">
+          <v-card-title class="text-center">Audio Transcription</v-card-title>
+          
+          <v-file-upload
+            accept="audio/*"
+            outlined
+            density="comfortable"
+            variant="outlined"
+            class="mt-4"
+            hide-details
+          ></v-file-upload>
+          
+          <v-divider class="my-4"></v-divider>
+          
+          <v-card-subtitle class="pb-0">Transcribed Text</v-card-subtitle>
+          <v-card-text class="pt-2">
+            <div v-if="transcribedText" class="text-body-1 pa-2 rounded bg-grey-lighten-4">{{ transcribedText }}</div>
+            <div v-else class="text-caption text-grey text-center pa-4">No transcribed text available</div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
-  //
+  import { ref } from 'vue';
+
+  const transcribedText = ref('');
+  transcribedText.value = 'Transcribed text will beaa displayed here.';
+
 </script>
