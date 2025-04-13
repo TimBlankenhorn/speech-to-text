c4aa41bdac9774cabdfff93cb0e8d9690450b26d
Tim <Timblankenhorn03@gmail.com>
2025-04-13 11:23:35 +0000

    added grafana and prometheus for monitoring

--- vm/backend/main.py
+++ vm/backend/main.py
@@ -1,11 +1,7 @@
-from typing import Union
-
 from fastapi import FastAPI
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from db import Database, Speech
-import tempfile
-import os
 from recognizer import Recognizer
 app = FastAPI()
 
@@ -17,17 +13,29 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+usedDBCounter = 0
+usedRecognizerCounter = 0
+
+#value for grafana dashboard
+@app.get("/origins")
+def get_origins():
+    return {
+        "DB": usedDBCounter,
+        "Recognizer": usedRecognizerCounter
+    }
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
+ 
 @app.post("/transcribe/")
 async def transcribe_audio(audio_file: UploadFile):
+    global usedDBCounter
+    global usedRecognizerCounter
+    
     ending = audio_file.filename.lower().split(".")[-1]
     valid_types = ["wav"]
     if not ending in valid_types:
@@ -41,6 +49,7 @@ async def transcribe_audio(audio_file: UploadFile):
         print("Hash exists in database")
         text = speech.content
         origin = "database"
+        usedDBCounter += 1
     else:
         
         print("Hash does not exist in database")
@@ -50,6 +59,7 @@ async def transcribe_audio(audio_file: UploadFile):
         new_speech = Speech(file_hash=file_hash, content=text)
         database.add_to_database(new_speech)
         origin = "speech recognizer"
+        usedRecognizerCounter += 1
     print(text)
     
     return {
